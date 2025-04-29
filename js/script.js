903621e2ec9aea64f537a266ba0cbedd87ff6afe
unomyayi <114829805+unomyayi@users.noreply.github.com>
2025-04-29 16:13:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Простой скрипт проверки статуса (пример)
+document.addEventListener("DOMContentLoaded", () => {
+    const statusEl = document.querySelector('.status');
+    if (statusEl) statusEl.textContent = "онлайн";
+  });
+  
